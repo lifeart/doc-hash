c79a3f0629246131caa1321a5ad71e316cb906c3
Alex Kanunnikov <lifeart92@gmail.com>
2024-05-14 16:34:37 +0300

    +

--- src/utils/document-creator.ts
+++ src/utils/document-creator.ts
@@ -2,7 +2,7 @@ import { type User } from './constants';
 import { type DocumentDTO, type FileDTO } from './file-manager';
 import { t } from './t';
 
-type AssuranceSheetData = {
+export type AssuranceSheetData = {
   hashFunction: string;
   users: User[];
   doc: DocumentDTO;
@@ -20,12 +20,12 @@ export async function createAssuranceSheet(data: AssuranceSheetData) {
     WidthType,
     BorderStyle,
     AlignmentType,
-    HeadingLevel,
+    // HeadingLevel,
     // TextRun,
     // ShadingType,
-    PatchType,
+    // PatchType,
     TextRun,
-    patchDocument,
+    // patchDocument,
     VerticalAlign,
   } = await import('docx');
 
@@ -47,163 +47,6 @@ export async function createAssuranceSheet(data: AssuranceSheetData) {
       ]
    */
   
-
-  fetch('./template_1.docx').then(async (response) => {
-    const fileData = await response.blob();
-    patchDocument(fileData,{
-      patches: {
-        hash_function_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(data.hashFunction),
-          ]
-        },
-        last_modified_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.last_modified),
-          ]
-        },
-        file_size_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.file_size),
-          ]
-        },
-        checksum_value_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.checksum_value),
-          ]
-        },
-        work_type_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.work_type),
-          ]
-        },
-        surname_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.surname),
-          ]
-        },
-        signature_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.signature),
-          ]
-        },
-        signing_date_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.signing_date),
-          ]
-        },
-        file_name_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.file_name),
-          ]
-        },
-        assurance_sheet_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.assurance_sheet),
-          ]
-        },
-        sheets_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.sheets),
-          ]
-        },
-        sheet_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.sheet),
-          ]
-        },
-        object_name_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.object_name),
-          ]
-        },
-        object_name_value: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(data.doc.objectName),
-          ]
-        },
-        serial_number_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.serial_number),
-          ]
-        },
-        serial_number_value: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(String(data.doc.serialNumber)),
-          ]
-        },
-        document_designation_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.document_designation),
-          ]
-        },
-        document_designation_value: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(data.doc.designation),
-          ]
-        },
-        document_name_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.document_name),
-          ]
-        },
-        document_name_value: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(data.doc.documentName),
-          ]
-        },
-        last_change_number_key: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(t.last_change_number),
-          ]
-        },
-        last_change_number_value: {
-          type: PatchType.PARAGRAPH,
-          children: [
-            new TextRun(String(data.doc.lastChangeNumber)),
-          ]
-        },
-      }
-    }).then(async (blob)=> {
-      const DocXBlob = new Blob([blob]);
-
-      const reader = new FileReader();
-    
-      await new Promise((resolve) => {
-        reader.onloadend = resolve;
-        reader.readAsDataURL(DocXBlob);
-      });
-    
-      // crate link to download
-      // const link = window.document.createElement('a');
-      // link.href = reader.result as string;
-      // link.download = 'template_1.docx';
-      // window.document.body.appendChild(link);
-      // link.click();
-      // window.document.body.removeChild(link);
-    });
-  });
   const document = new Document({
     styles: {
       default: {
@@ -219,11 +62,11 @@ export async function createAssuranceSheet(data: AssuranceSheetData) {
     sections: [
       {
         children: [
-          new Paragraph({
-            text: t.assurance_sheet,
-            heading: HeadingLevel.TITLE,
-            alignment: AlignmentType.CENTER,
-          }),
+          // new Paragraph({
+          //   text: t.assurance_sheet,
+          //   heading: HeadingLevel.TITLE,
+          //   alignment: AlignmentType.CENTER,
+          // }),
           createAssuranceTable(data),
         ],
       },
@@ -324,7 +167,7 @@ export async function createAssuranceSheet(data: AssuranceSheetData) {
     const columnWidth = 910; // 963
     const gridSize = 10;
     const columnWidths = [
-      columnWidth, columnWidth, columnWidth, columnWidth, columnWidth,
+      columnWidth + 200, columnWidth, columnWidth, columnWidth , columnWidth - 200,
       columnWidth, columnWidth, columnWidth - 100, columnWidth, columnWidth + 100
     ]
     const table = new Table({
