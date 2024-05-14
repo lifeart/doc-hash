c8b7a4a04687fa8fcdb0878a13d9f7dfc1be5ccd
Alex Kanunnikov <lifeart92@gmail.com>
2024-05-14 22:15:32 +0300

    +

--- src/tests/unit/hash-function-test.ts
+++ src/tests/unit/hash-function-test.ts
@@ -1,47 +1,58 @@
 import { module, test } from 'qunit';
 import { AlgorithmType, getHash } from '@/utils/constants';
+import { Progress } from '@/utils/progress';
 
 module('Unit | functions | getHash', function () {
   test('md5 works', async function (assert) {
     const file = new File([new Blob(['hello world'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.MD5);
+    const loader = new Progress(() => true, 1);
+    const hash = await getHash(file, AlgorithmType.MD5, loader);
     assert.equal(hash, '5eb63bbbe01eeed093cb22bb8f5acdc3');
   });
   test('md5 hash not cached', async function (assert) {
     const file = new File([new Blob(['hello worlds'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.MD5);
+    const loader = new Progress(() => true, 1);
+    const hash = await getHash(file, AlgorithmType.MD5, loader);
     assert.equal(hash, 'b96b878ad72f56709dbb5628e1cea18d');
   });
   test('sha1 works', async function (assert) {
     const file = new File([new Blob(['hello world'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.SHA1);
+    const loader = new Progress(() => true, 1);
+
+    const hash = await getHash(file, AlgorithmType.SHA1, loader);
     assert.equal(hash, '2aae6c35c94fcfb415dbe95f408b9ce91ee846ed');
   });
   test('sha1 not cached', async function (assert) {
     const file = new File([new Blob(['hello worlds'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.SHA1);
+    const loader = new Progress(() => true, 1);
+
+    const hash = await getHash(file, AlgorithmType.SHA1, loader);
     assert.equal(hash, 'b1b91b92e4ef0b0afa7f3448658781018ca3310c');
   });
   test('crc32 works', async function (assert) {
     const file = new File([new Blob(['hello world'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.CRC32);
+    const loader = new Progress(() => true, 1);
+
+    const hash = await getHash(file, AlgorithmType.CRC32, loader);
     assert.equal(hash, '0d4a1185');
   });
   test('crc32 not cached', async function (assert) {
     const file = new File([new Blob(['hello worlds'])], 'filename.txt', {
       type: 'text/plain',
     });
-    const hash = await getHash(file, AlgorithmType.CRC32);
+    const loader = new Progress(() => true, 1);
+
+    const hash = await getHash(file, AlgorithmType.CRC32, loader);
     assert.equal(hash, '86d1f2b5');
   });
 });
--- src/utils/progress.ts
+++ src/utils/progress.ts
@@ -1,3 +1,4 @@
+import 'decorator-transforms/globals'; // workaround for test bundling
 import { tracked } from '@lifeart/gxt';
 export class Progress {
   constructor(isActual: () => boolean, totalChunks: number) {
@@ -9,6 +10,7 @@ export class Progress {
   totalChunks = 0;
   get percents() {
     const value = this.totalChunks ? (this.currentChunk / this.totalChunks) * 100 : 0;
+    if (!this.isActual() || value >= 100) return '100.00';
     return value.toFixed(2);
   }
   
